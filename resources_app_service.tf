368f5a4d4679e6aa0fcf19cd2d4e05daa0cf53ac
Kamen Yovchev <kamen.yovchev@wize-soluions.com>
2020-10-23 15:48:12 +0300

    add new version in web settings

--- resources_app_service.tf
+++ resources_app_service.tf
@@ -8,7 +8,7 @@ resource "azurerm_app_service" "app_service" {
 
 
   site_config {
-    linux-fx-version = "DOTNETCORE|2.1"
+    linux_fx_version = "DOTNETCORE|2.1"
     scm_type = "LocalGit"
     always_on = true
     managed_pipeline_mode = "Integrated"
