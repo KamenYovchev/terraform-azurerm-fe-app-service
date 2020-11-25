a4215772df4e1d33b71f4bcfe56ec00262ed3a98
Kamen Yovchev <kamen.yovchev@wize-soluions.com>
2020-11-25 17:41:30 +0200

    remove values

--- resources_app_service.tf
+++ resources_app_service.tf
@@ -5,5 +5,14 @@ resource "azurerm_app_service" "app_service" {
   resource_group_name = var.resource_group_name
   app_service_plan_id = var.app_service_plan_id
   tags                = local.common_tags
+  client_affinity_enabled = false
 
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type = "LocalGit"
+    always_on = true
+    managed_pipeline_mode = "Integrated"
+    ftps_state = "Disabled"
+    websockets_enabled = false
+  }
 }
